da5a82986652bc89c49cac0a6692ba69f9eeebb6
sandwichfe <1344382941@qq.com>
2022-08-17 15:53:21 +0800

    backtop样式

--- src/components/backTop/BackTop.vue
+++ src/components/backTop/BackTop.vue
@@ -1,29 +1,26 @@
 <template>
-    <div class="back-top">
-
-        <svg-icon
-            iconClass="backtop"
-            className="bt-icon"
-          ></svg-icon>
-
-
-    </div>
+  <div class="back-top">
+    <svg-icon iconClass="backtop" className="bt-icon"></svg-icon>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "BackTop"
-    }
+export default {
+  name: "BackTop",
+};
 </script>
 
 <style scoped>
-    .back-top {
-        position: fixed;
-        right: 12%;
-        bottom: 5rem;
-    }
-    .back-top .bt-icon {
-        width: 43px;
-        height: 43px;
-    }
+.back-top {
+  position: fixed;
+  right: 12%;
+  bottom: 80px;
+}
+.back-top .bt-icon {
+  width: 43px;
+  height: 43px;
+  fill: currentColor;
+  color: #c8dae2;
+  opacity: 0.4;
+}
 </style>
