fe197c123e08dd43992b10f9034f791a5f1acd70
sandwichfe <1344382941@qq.com>
2022-08-18 10:19:19 +0800

    移动端样式

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,17 @@
     div {
      -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
     }
+    /*清除ios下的默认圆角*/
+input,
+button {
+  -webkit-appearance: none;
+  border-radius: 0;
+}
+body {
+  -webkit-text-size-adjust: 100%;/*禁止文字缩放*/
+}
+body * {
+  font-family: helvetica; /*默认字体设置*/
+}
+
 </style>
